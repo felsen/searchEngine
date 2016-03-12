feabd6087f063f566eecad3c52016b12a0d704f4
Sourav <roysourav1990@gmail.com>
2016-03-12 12:57:21 +0530

    Removed duplicate

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/sourav/9addc052-62c7-43ab-9fa5-5813f5304de4/workspace/GITHUB/searchEngine/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
