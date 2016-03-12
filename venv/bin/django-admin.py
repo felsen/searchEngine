16ca29268c4e8c0edab6406906125b9d68c5d451
Sourav <roysourav1990@gmail.com>
2016-03-12 12:24:13 +0530

    Added Procfile

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/sourav/9addc052-62c7-43ab-9fa5-5813f5304de4/workspace/GITHUB/searchEngine/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
